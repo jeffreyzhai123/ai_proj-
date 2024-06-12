770179e362733209e77377561578d3b0c7c4f381
jeffreyzhai123 <jeffreygxzhai@gmail.com>
2024-06-12 16:43:28 -0700

    idk what i am doing

--- problembank.js
+++
@@ -1,4 +0,0 @@
-export const q1 = "def foo(a,b): return a + b";
-export const q2 = "def bar(a, b): return a - b";
-export const q3 = "def ping(a, b): return a * b";
-export const q4 = "def pong(a, b): return a / b";
