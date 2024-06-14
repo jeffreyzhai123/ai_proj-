b28ef3af2a53ff099d0348925918308660e44794
AndyC <ychen276@students.cs.ubc.ca>
2024-06-13 22:54:17 -0700

    conceal dropbar if not logged in

--- backend/routes/submit.js
+++ backend/routes/submit.js
@@ -7,7 +7,7 @@ let inputVal = 1;
 router.post('/', (req, res) => {
     const userInput = req.body; // Access the parsed JSON data from the request body
     console.log('Received input:', userInput); // Log the received data to the console
-    inputVal = parseInt(userInput.inputA); //access the input property of the json obj and parse the string into an int
+    inputVal = parseInt(userInput.userSelection); //access the input property of the json obj and parse the string into an int
     const responseString = selector(inputVal);
     res.json({ message: responseString }); // Send a response back to the client (changed plain string into a json object)
 });
--- frontend/src/Test.js
+++ frontend/src/Test.js
@@ -1,80 +1,58 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import DropDown from './dropDown.js';
+import SubmissionForm from './submitForm.js';
 import { useUser } from '@clerk/clerk-react'
+
 //should add useEffect later on to deal with having to make api get calls to request data from backend
 
 function Test() {
-  const [inputA, setInputA] = useState(''); // State to store user input
-  const [inputB, setInputB] = useState('');
-  const [responseA, setResponseA] = useState('');
-  const [responseB, setResponseB] = useState('');
-  const { user } = useUser()
-  const navigate = useNavigate()
+
+  const [userSelection, setUserSelection] = useState(null);
+  const [result, setResult] = useState("");
+  const navigate = useNavigate();
+  const { user }  = useUser();
 
   const exitButton = () => {
-    navigate("/")
+    navigate("/");
+  }
+  
+  const handleUserResponse = (data) => {
+    setUserSelection(data.message);
   }
 
-  const handleChangeA = (e) => {
+  const handleResult = (ans) => {
     if (user) {
-      setInputA(e.target.value); // Update state with the current input value
-    }
-  };
-
-  const handleChangeB = (e) => {
-    if (user) {
-      setInputB(e.target.value); // Update state with the current input value
-    }
-  };
-
-  const handleSubmitA = async (e) => { //e is event object
-    if (user) {
-      e.preventDefault(); // Prevent the default form submission behavior
-
-      // Send a POST request to the backend server
-      try {
-        setResponseA(''); //set response state to an empty string each time 
-
-        const response = await fetch('http://localhost:3080/submit', {
-          method: 'POST', // Specify the HTTP method (POST)
-          headers: {
-            'Content-Type': 'application/json', // Set the Content-Type header to JSON
-          },
-          body: JSON.stringify({ inputA }), // Convert the input state to a JSON string
-        });
-
-        const data = await response.json(); // Get the response text
-        setResponseA(data.message); //updates the state of the app component
-      } catch (error) {
-        console.error('Error submitting data:', error);
-      }
-    }
-  };
-
-  const handleSubmitB = async (e) => {
-    if (user) {
-      e.preventDefault();
-
-      try {
-        setResponseB('');
-
-        const response = await fetch('http://localhost:3080/answer', {
-          method: 'POST', 
-          headers: {
-            'Content-Type': 'application/json', 
-          },
-          body: JSON.stringify({ inputB }), 
-        });
-        const data = await response.json(); // Get the response text
-        setResponseB(data.message); //updates the state of the app component
-      } catch (error) {
-        console.error('Error submitting data:', error);
-      }
+      setResult(ans);
     }
   }
-
+  
   return (
     <div>
+      {/* Dropdown menu for question selection */}
+      <div>
+        <h1>Question Selector</h1>
+        <DropDown onUserDataReceived={handleUserResponse}/>
+      </div>
+
+      {/* Submission form, uses conditional rendering (only renders if userSelection is properly updated */}
+      {userSelection && (
+        <div className="response-container">
+          <h2>
+            {userSelection}
+          </h2>
+          <SubmissionForm onResultReceived={handleResult}/>
+        </div>
+      )}
+
+      {/* Conditional render to display results */}
+      {result && (
+        <div className='result-container'>
+          <p>{result.code}</p>
+          <div dangerouslySetInnerHTML={{ __html: result.test }} />
+        </div>
+      )}
+
       <div className={'buttonContainer'}>
         <input
           className={'inputButton'}
@@ -83,40 +61,9 @@ function Test() {
           value={"Go Back"}
         />
       </div>
-      {/* Form A: question selection */}
-      <h1>Select a number between 1 and 4 inclusive</h1>
-      <form onSubmit={handleSubmitA}>
-        <label>
-          Input:
-          <input type="text" value={inputA} onChange={handleChangeA} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
 
-      {/* Form B: question display, shows only after receiving a valid response */}
-      {responseA && ( //response && is a conditoional rendering where this component only gets rendered if the var response is not null
-        <div className="response-container">
-          <form onSubmit={handleSubmitB}>
-            <h2>
-              {responseA}
-            </h2>
-            <label>
-              Answer:
-              <input type="text" value={inputB} onChange={handleChangeB} />
-              <button type="submit">Submit</button>
-            </label>
-          </form>
-        </div>
-      )}
-
-      {responseB && (
-        <div className='result-container'>
-          <p>{responseB}</p>
-        </div>
-      )}
     </div>
   );
 }
-
 
 export default Test;
